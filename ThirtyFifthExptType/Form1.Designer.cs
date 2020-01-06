e05f8df82600f0bfa4bd69101b96bdb57a3483c7
RedHolla <abhiholla2012@gmail.com>
2020-01-06 17:50:08 +0530

    qwe

---
+++ ThirtyFifthExptType/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace ThirtyFifthExptType
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.img_disp = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_disp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "To Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(18, 348);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(508, 109);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "For Checking";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(419, 25);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(178, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Apply";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(115, 68);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(59, 25);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(26, 73);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(92, 16);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Set Threshold";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Draw Rectangle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(601, 434);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(432, 330);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // img_disp
+            // 
+            this.img_disp.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.img_disp.Location = new System.Drawing.Point(624, 12);
+            this.img_disp.Margin = new System.Windows.Forms.Padding(2);
+            this.img_disp.Name = "img_disp";
+            this.img_disp.Size = new System.Drawing.Size(228, 177);
+            this.img_disp.TabIndex = 34;
+            this.img_disp.TabStop = false;
+            this.img_disp.Click += new System.EventHandler(this.img_disp_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox2.Location = new System.Drawing.Point(624, 224);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(228, 177);
+            this.pictureBox2.TabIndex = 35;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(309, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(64, 15);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "Pixel Count";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(379, 67);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(81, 30);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 469);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.img_disp);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Experimenting";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_disp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox img_disp;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ ThirtyFifthExptType/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+
+
+namespace ThirtyFifthExptType
+{
+    public partial class Form1 : Form
+    {
+        bool draw=false;
+        bool IsMouseDown=false;
+        bool imgsave = false;
+        Rectangle rect;
+        OpenCvSharp.Point LocationXY;
+        OpenCvSharp.Point LocationX1Y1;
+        Rect v;
+        Mat imgInput = new Mat();
+        Mat templ1=new Mat();
+        Mat result = new Mat();
+        int i = 0;
+        int tx, ty, th, tw;
+        int var_ind = 0;
+        int countpixel = 0;
+        Rect Rect_crop;
+        bool[] mousedraw = new bool[5];
+        int[] Pos_tx = new int[5];
+        int[] Pos_ty = new int[5];
+        int[] Pos_tw = new int[5];
+        int[] Pos_th = new int[5];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+                textBox1.Text = openFileDialog1.FileName;
+                progressBar1.Value = 50;
+                imgInput = new Mat(openFileDialog1.FileName, ImreadModes.Color);
+            }
+            else
+            {
+                textBox1.Text = "No picture selected";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            draw = true;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {}
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {}
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (rect != null)
+            {
+                e.Graphics.DrawRectangle(Pens.Red, GetRect());
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {}
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {}
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {}
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (draw)
+            {
+                if (rect != null)
+                {
+                    e.Graphics.DrawRectangle(Pens.Red, GetRect());
+                }
+            }
+        }
+
+        private Rectangle GetRect()
+        {
+            rect = new Rectangle();
+            rect.X = Math.Min(LocationXY.X, LocationX1Y1.X);
+            rect.Y = Math.Min(LocationXY.Y, LocationX1Y1.Y);
+            rect.Width = Math.Abs(LocationXY.X - LocationX1Y1.X);
+            rect.Height = Math.Abs(LocationXY.Y - LocationX1Y1.Y);
+            return rect;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (draw)
+            {
+                IsMouseDown = true;
+                LocationXY = new OpenCvSharp.Point(e.X,e.Y);
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draw)
+            {
+                if (IsMouseDown == true)
+                {
+                    LocationX1Y1 = new OpenCvSharp.Point(e.X, e.Y);
+                    pictureBox1.Invalidate();
+                }
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {}
+
+        private void img_disp_Click(object sender, EventArgs e)
+        {}
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {}
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.Description = "Where do you want to store the images?";
+            folderBrowserDialog1.ShowDialog();
+            if(folderBrowserDialog1.SelectedPath=="")
+            {
+                MessageBox.Show("Cannot log images", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            imgsave = true;
+            if (imgsave)
+            {
+                Cv2.ImWrite(folderBrowserDialog1.SelectedPath + "\\" + i.ToString() + ".bmp", imgInput);
+            }
+            i++;
+        }
+        
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {}
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {}
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (draw)
+            {
+                if (IsMouseDown == true)
+                {
+                    LocationX1Y1 = new OpenCvSharp.Point(e.X, e.Y);
+                    IsMouseDown = false;
+                    if(rect!=null)
+                    {
+                        double scaleX = (double)imgInput.Width / pictureBox1.Size.Width;
+                        double scaleY = (double)imgInput.Height / pictureBox1.Size.Height;
+                        tx = (int)(rect.X * scaleX) + 5 ;
+                        ty = (int)(rect.Y * scaleY) + 10;
+                        tw = (int)(rect.Width * scaleX);
+                        th = (int)(rect.Height * scaleY);
+                        OpenCvSharp.Rect t = new OpenCvSharp.Rect(tx, ty, tw, th);
+                        Mat temp = new Mat(imgInput, t);
+                        //if(mousedraw[var_ind]==true)
+                        
+                            Pos_tx[var_ind] = tx;
+                            Pos_ty[var_ind] = ty;
+                            Pos_tw[var_ind] = tw;
+                            Pos_th[var_ind] = th;
+                        
+                        Bitmap bmp = BitmapConverter.ToBitmap(temp);
+                        img_disp.Image = bmp;
+                        img_disp.SizeMode = PictureBoxSizeMode.StretchImage;
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            process_image(new Mat(openFileDialog1.FileName, ImreadModes.Color), true);
+        }
+
+        private void process_image(Mat imgIn, bool offline = false)
+        {
+            Rect_crop = new Rect(tx, ty, tw, th);
+            Mat tempa = new Mat();
+            imgIn.CopyTo(imgInput);
+            tempa = new Mat(imgInput, Rect_crop);
+            
+            Cv2.CvtColor(tempa, tempa, ColorConversionCodes.BGR2GRAY);
+            Cv2.Threshold(tempa, tempa, (int)numericUpDown1.Value, 255, ThresholdTypes.Binary);
+            Cv2.NamedWindow("s", WindowMode.Normal);
+            Cv2.ImShow("s", tempa);
+            Bitmap bmp1 = BitmapConverter.ToBitmap(tempa);
+            pictureBox2.Image = bmp1;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+           
+
+            for (int x = 0; x < tempa.Rows; x++)
+            {
+                for (int y = 0; y < tempa.Cols; y++)
+                {
+                    if (tempa.At<char>(x, y) == 255)
+                    {
+                        countpixel++; 
+                    }
+                }
+            }
+            textBox4.Text = countpixel.ToString();
+        }
+    }
+}
